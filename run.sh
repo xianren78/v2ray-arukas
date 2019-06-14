c5a2bdbf56282d4353baf09431b6a4c0bfa66cfb
中男顽童 <qgtxqj@gmail.com>
2019-06-14 16:48:29 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,24 +21,16 @@ sed -i "s/uu6id/$UU6ID/g" /etc/v2ray/config.json
 sed -i "s/sspass/$SSPASS/g" /etc/v2ray/config.json
 cd /tmp
 wget --no-check-certificate https://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
-unzip v2ray-linux-64.zip "v2ray" -d /usr/bin/v2ray/
-unzip v2ray-linux-64.zip "v2ctl" -d /usr/bin/v2ray/
-unzip v2ray-linux-64.zip "geosite.dat" -d /usr/bin/v2ray/
-unzip v2ray-linux-64.zip "geoip.dat" -d /usr/bin/v2ray/
-
+unzip v2ray-linux-64.zip v2ray v2ctl geosite.dat geoip.dat -d /usr/bin/v2ray/
 if [ ! -f "/usr/bin/v2ray/v2ray" ]; then
- unzip v2ray.zip "v2ray-v$VER-linux-64\v2ray" -d /usr/bin/v2ray/
- unzip v2ray.zip "v2ray-v$VER-linux-64\v2ctl" -d /usr/bin/v2ray/
- unzip v2ray.zip "v2ray-v$VER-linux-64\geosite.dat" -d /usr/bin/v2ray/
- unzip v2ray.zip "v2ray-v$VER-linux-64\geoip.dat" -d /usr/bin/v2ray/
+ unzip v2ray-linux-64.zip "v2ray-v$VER-linux-64\v2ray" "v2ray-v$VER-linux-64\v2ctl" "v2ray-v$VER-linux-64\geosite.dat" "v2ray-v$VER-linux-64\geoip.dat" -d /usr/bin/v2ray/
 fi
 rm -rf /tmp/v2ray-linux-64.zip
-cd /usr/bin/v2ray
-chmod +x v2ray v2ctl
+chmod +x /usr/bin/v2ray/v2ray /usr/bin/v2ray/v2ctl
+
 /usr/bin/v2ray/v2ray -config=/etc/v2ray/config.json >/dev/null  2>&1  &
 echo "Start v2ray Success!"
 
-cd /tmp
 wget --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh
 /usr/local/caddy/caddy -conf /etc/caddy/Caddyfile >/dev/null  2>&1  &
 
@@ -50,5 +42,4 @@ sed -i '$i 0       12      *       *       *       cat /dev/null > /etc/caddy/ca
 crond
 echo "Start crond Success!"
 
-cd /usr/bin/v2ray
-./restart.sh
+/usr/bin/v2ray/restart.sh
